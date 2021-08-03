22a4e29adce33177a6f81fb47e8d32d06f83ab3a
Vitaly Litvinov <vedun.ufa@gmail.com>
2021-08-04 02:56:54 +0300

    #2 added proto submodule, added proto compiler, modifed cmake files

--- cmake/UsefulMacros.cmake
+++ cmake/UsefulMacros.cmake
@@ -1,3 +1,40 @@
+if(WIN32)
+	set(CMAKE_FIND_LIBRARY_SUFFIXES "" ".dll" ".lib" ".a")
+endif()
+
+string( TOUPPER ${CMAKE_BUILD_TYPE} CMAKE_BUILD_TYPEUP)
+
+if(CMAKE_BUILD_TYPEUP STREQUAL DEBUG AND WIN32)
+	set(BICYCLE_DEBUG_POSTFIX "d")
+endif()
+
+
+if(NOT SPEC OR NOT HOST)
+	if(CMAKE_SYSTEM_NAME STREQUAL "Linux")
+		set(HOST linux)
+		if(CMAKE_SYSTEM_PROCESSOR STREQUAL "i686")
+			set(SPEC linux)
+		elseif(CMAKE_SYSTEM_PROCESSOR STREQUAL "armv")
+			set(SPEC arm)
+			add_compile_definitions(TRITONN_YOCTO)
+		elseif(CMAKE_SYSTEM_PROCESSOR STREQUAL "x86_64")
+			set(SPEC linux)
+		endif()
+	elseif(CMAKE_SYSTEM_NAME STREQUAL "Windows")
+		set(HOST "win32")
+		if(MSVC)
+			if(CMAKE_CL_64)
+				set(SPEC "msvc")
+			else()
+				set(SPEC "msvc32")
+			endif()
+		else()
+			set(SPEC "mingw")
+		endif()
+	endif()
+endif()
+
+### MACRO #########################################################################################
 
 macro( add_lib projectname )
 
@@ -84,4 +121,96 @@ macro( set_project_version FILE_IN)
 	string( FIND ${FILE_IN} ".in" VER_POS)
 	string( SUBSTRING ${FILE_IN} 0 ${VER_POS} FILE_OUT)
 	configure_file("${CMAKE_SOURCE_DIR}/${FILE_IN}" "${CMAKE_BINARY_DIR}/${FILE_OUT}")
+endmacro()
+
+function (ListToString result delim)
+	list(GET ARGV 2 temp)
+	math(EXPR N "${ARGC}-1")
+	foreach(IDX RANGE 3 ${N})
+		list(GET ARGV ${IDX} STR)
+		set(temp "${temp}${delim}${STR}")
+	endforeach()
+	set(${result} "${temp}" PARENT_SCOPE)
+endfunction(ListToString)
+
+macro( compile_proto PROJECTNAME)
+	file( GLOB_RECURSE protos "*.proto" )
+	file( GLOB_RECURSE protosin "*.proto.in" )
+
+	foreach( proto ${exclude_proto} )
+		list( REMOVE_ITEM protos ${proto} )
+	endforeach()
+
+	list(APPEND protos ${add_proto})
+
+	foreach( protoin ${exclude_protoin} )
+		list( REMOVE_ITEM protosin ${protoin} )
+	endforeach()
+
+	list(APPEND protosin ${add_protoin})
+
+	foreach(protoin_item ${protosin})
+		get_filename_component(protoin_file_name ${protoin_item} NAME_WE)
+
+		if(PROTOBUF_FULL_LIB)
+			set(PROTOBUF_FULL_LIB_STRING "")
+		else()
+			set(PROTOBUF_FULL_LIB_STRING "option optimize_for = LITE_RUNTIME;")
+		endif()
+
+		set(dest_protoin_file ${${PROJECTNAME}_BINARY_DIR}/${protoin_file_name}.proto)
+		configure_file(${protoin_item} ${dest_protoin_file} @ONLY)
+		set_property(SOURCE ${dest_protoin_file} PROPERTY GENERATED ON)
+		list(APPEND protos ${dest_protoin_file})
+	endforeach()
+
+	list(LENGTH protos protos_len)
+
+	if(protos_len GREATER 0)
+#		list( APPEND srcs ${protos})
+		foreach(proto_item ${protos})
+			get_filename_component(proto_item_NAME ${proto_item} NAME_WE)
+			get_filename_component(proto_item_DIR ${proto_item} PATH)
+
+			set(protos_out ${${PROJECTNAME}_BINARY_DIR}/${proto_item_NAME}.pb.cc ${${PROJECTNAME}_BINARY_DIR}/${proto_item_NAME}.pb.h)
+			get_property(a3 SOURCE ${proto_item} PROPERTY GENERATED)
+
+			list(APPEND proto_path "--proto_path=${PROTOBUF_INCLUDE_DIR}")
+
+			if (a3)
+				list(APPEND proto_path "--proto_path=${proto_item_DIR}" "--proto_path=${${PROJECTNAME}_BINARY_DIR}" "--proto_path=${${PROJECTNAME}_SOURCE_DIR}")
+				foreach(lib ${libs} )
+					if(${lib} MATCHES ${PROJECT_NAME_GLOBAL} )
+						list(APPEND proto_path "--proto_path=${${lib}_BINARY_DIR}" "--proto_path=${${lib}_SOURCE_DIR}")
+					endif()
+				endforeach()
+			else()
+				set(proto_path "--proto_path=${proto_item_DIR}")
+			endif()
+
+			list(APPEND protos_out_all ${protos_out})
+			list(REMOVE_DUPLICATES proto_path)
+			list(APPEND srcs ${protos_out})
+			set_source_files_properties(${protos_out} PROPERTIES GENERATED TRUE)
+
+			#ListToString(proto_path_str, " ", ${proto_path})
+			#string(REPLACE ";" " " proto_path_str "${proto_path}")
+			#string(REPLACE ";" " " protos_out_str "${protos_out}")
+
+			message(STATUS "PROTOBUF: ======================================")
+			message(STATUS "COMMAND: ${PROTOBUF_COMPILER} ${proto_path_str} --cpp_out=\"${${PROJECTNAME}_BINARY_DIR}\" ${proto_item}")
+			message(STATUS "proto_item: ${proto_item}")
+			message(STATUS "protos_out: ${protos_out}")
+
+			add_custom_command(
+				COMMAND ${PROTOBUF_COMPILER}
+				ARGS ${proto_path} --cpp_out="\"${${PROJECTNAME}_BINARY_DIR}\"" ${proto_item}
+				DEPENDS ${proto_item}
+				OUTPUT ${protos_out}
+			)
+		endforeach()
+
+		add_custom_target(${PROJECTNAME}_protobuf_autogen ALL DEPENDS ${protos_out_all})
+	endif()
+
 endmacro()
