a3d73f85c06ab416eeab54ad0e015bd717dddf61
Litvinov Vitaly <vedun@mail.ru>
2021-09-21 23:16:50 +0300

    #52 wip

--- src/tests/test_stream.cpp
+++ src/tests/test_stream.cpp
@@ -37,7 +37,7 @@ TEST_CASE("testing stream calc.", "[Stream]")
 		ss.add("sikn1.line2.factors.set.accept"     , 1);
 		ss.set();
 
-		mSleep(1800);
+		mSleep(1900);
 
 		ss.clear();
 		ss.add("sikn1.line2.temperature.value");
@@ -65,6 +65,60 @@ TEST_CASE("testing stream calc.", "[Stream]")
 		CHECK  (ss("sikn1.line2.total.present.mass")->getValueLREAL() == 2.0);
 		CHECK  (ss("sikn1.line2.total.present.impulse")->getValueUDINT() == freq * 2);
 		CHECK  (ss("sikn1.line2.flowrate.mass.value")->getValueLREAL() == 3600);
+
+		Catch::Epsilon::instance().setDouble(epsilon);
+	}
+
+	SECTION("Check calculating flowrate. const KF") {
+		rSnapshot ss(rDataManager::instance().getVariableClass(), ACCESS_MASK_SA);
+		LREAL freq  = 300.0;
+		LREAL dens15 = 830.0;
+		LREAL b15    = 0.00089124;
+		LREAL y15    = 0.00076979;
+		LREAL dens   = 819.8323017886;
+		LREAL temp   = 25.0;
+		LREAL pres   = 1.1;
+
+		ss.add("hardware.fi4_1.ch_01.simulate.type" , static_cast<USINT>(rIOFIChannel::SimType::CONST));
+		ss.add("hardware.fi4_1.ch_01.simulate.value", freq);
+		ss.add("var.test_dens15.value"              , dens15);
+		ss.add("var.test_b15.value"                 , b15);
+		ss.add("sikn1.line1.io.temp.present.value"  , temp);
+		ss.add("sikn1.line1.io.pres.present.value"  , pres);
+		ss.add("sikn1.line1.linearization"          , 0);
+		ss.add("sikn1.line1.total.reset"            , 1);
+		ss.add("sikn1.line1.factors.set.kf"         , 100);
+		ss.add("sikn1.line1.factors.set.accept"     , 1);
+		ss.set();
+
+		mSleep(1900);
+
+		ss.clear();
+		ss.add("sikn1.line1.temperature.value");
+		ss.add("sikn1.line1.pressure.value");
+		ss.add("sikn1.line1.obj.stn_dens.density.value");
+		ss.add("sikn1.line1.total.present.mass");
+		ss.add("sikn1.line1.total.present.impulse");
+		ss.add("sikn1.line1.flowrate.mass.value");
+		ss.add("sikn1.line1.obj.stn_dens.density.value");
+		ss.get();
+
+		LREAL epsilon = Catch::Epsilon::instance().setDouble(0.0001);
+
+		REQUIRE(ss("sikn1.line1.temperature.value"));
+		REQUIRE(ss("sikn1.line1.pressure.value"));
+		REQUIRE(ss("sikn1.line1.obj.stn_dens.density.value"));
+		REQUIRE(ss("sikn1.line1.obj.stn_dens.density.value"));
+		REQUIRE(ss("sikn1.line1.total.present.mass"));
+		REQUIRE(ss("sikn1.line1.total.present.impulse"));
+		REQUIRE(ss("sikn1.line1.flowrate.mass.value"));
+
+		CHECK  (ss("sikn1.line1.temperature.value")->getValueLREAL() == temp);
+		CHECK  (ss("sikn1.line1.pressure.value")->getValueLREAL() == pres);
+		CHECK  (ss("sikn1.line1.obj.stn_dens.density.value")->getValueLREAL() == dens);
+		CHECK  (ss("sikn1.line1.total.present.mass")->getValueLREAL() == 2.0);
+		CHECK  (ss("sikn1.line1.total.present.impulse")->getValueUDINT() == freq * 2);
+		CHECK  (ss("sikn1.line1.flowrate.mass.value")->getValueLREAL() == 3600);
 
 		Catch::Epsilon::instance().setDouble(epsilon);
 	}
