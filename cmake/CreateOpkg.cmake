6ff87237ae55b9178bf61f8522398f4f1bdd283d
Vitaly Litvinov <vedun.ufa@gmail.com>
2021-10-25 01:46:16 +0300

    #117 an attempt to make a custom target and a cpack work together

--- cmake/CreateOpkg.cmake
+++ cmake/CreateOpkg.cmake
@@ -1,18 +1,21 @@
 # create .bin file
 
 if(CMAKE_TRITONN_YOCTO)
-	set(CPACK_PACKAGE_VERSION "${PROJECT_VERSION_MAJOR}.${PROJECT_VERSION_MINOR}.${PROJECT_VERSION_BUILD}.${PROJECT_VERSION_HASH}")
+	set(CPACK_PACKAGE_VERSION "${PROJECT_VERSION_STRING}")
 	set(CPACK_TRITONN_FILENAME "${PROJECT_NAME}_${CPACK_PACKAGE_VERSION}")
 	set(CPACK_GENERATOR "ZIP")
 	set(CPACK_SET_DESTDIR ON)
-	set(CPACK_PACKAGE_FILE_NAME "CPACK_TRITONN_FILENAME")
+	set(CPACK_PACKAGE_FILE_NAME "${CPACK_TRITONN_FILENAME}")
 	set(CPACK_INCLUDE_TOPLEVEL_DIRECTORY OFF)
+
+	message(STATUS "Create package: ${CPACK_TRITONN_FILENAME}.zip")
 
 	include(CPack)
 
-	add_custom_target(package_ssl
-		COMMAND openssl enc -e -aes256 -pbkdf2 -k Kx48dHnZ@o3nE8L%pfNu3V9rM8g@n7 -in ${CPACK_TRITONN_FILENAME}.zip -out ${CPACK_TRITONN_FILENAME}.bin
+	add_custom_target(package_ssl COMMAND
+		openssl enc -e -aes256 -pbkdf2 -k Kx48dHnZ@o3nE8L%pfNu3V9rM8g@n7 -in ${CPACK_TRITONN_FILENAME}.zip -out ${CPACK_TRITONN_FILENAME}.bin
 		DEPENDS package
 	)
+#	add_dependencies(package_ssl package)
 
 endif()
